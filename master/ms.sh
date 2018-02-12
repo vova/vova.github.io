4b90e5ff258947d12ed974b388b938728103b3b2
Vova <vova@users.noreply.github.com>
2018-02-12 19:00:13 +0200

    add master/ms.sh which was shortened wrong

--- master/ms.sh
+++ master/ms.sh
@@ -1,1 +1,18 @@
-<html><head><meta http-equiv="refresh" content="0; url=https://vova.github.io/" /></head></html>
+#!/bin/bash
+
+# minimun setup of vim with rails-related stuff
+# to be run with 
+# curl https://raw.githubusercontent.com/vova/vova.github.io/master/ms.sh | bash
+# or (with url shortened) 
+# curl -L git.io/vvim | bash
+
+sudo gem install ripper-tags
+sudo apt-get install silversearcher-ag
+wget https://raw.githubusercontent.com/vova/vova.github.io/master/.vimrc -O ~/.vimrc
+echo "alias be='bundle exec'" >> ~/.bashrc
+echo "alias eg='vim Gemfile'" >> ~/.bashrc
+echo "alias gg='ripper-tags -R --exclude=.git --exclude=vendor .'" >> ~/.bashrc
+echo "alias ivm='vim'" >> ~/.bashrc
+echo "alias rc='vim ~/.bashrc && source ~/.bashrc'" >> ~/.bashrc
+echo "export RAILS_ENV=" >> ~/.bashrc
+vim ~/.bashrc && source ~/.bashrc
