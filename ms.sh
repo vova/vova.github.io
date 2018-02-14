ee837d795ab8e54e8660f3196cf50cc7a7ea98f5
Vova <vova@users.noreply.github.com>
2018-02-14 09:14:19 +0200

    add backing up .vimrc before dling it

--- ms.sh
+++ ms.sh
@@ -8,12 +8,15 @@
 
 sudo gem install ripper-tags
 sudo apt-get install silversearcher-ag
+[ -f ~/.vimrc ] && mv ~/.vimrc ~/.vimrc.old || echo ".vimrc Not found"
 wget https://raw.githubusercontent.com/vova/vova.github.io/master/.vimrc -O ~/.vimrc
 echo "alias be='bundle exec'" >> ~/.bashrc
+echo "alias e='vim'" >> ~/.bashrc
 echo "alias eg='vim Gemfile'" >> ~/.bashrc
 echo "alias f='find . -name'" >> ~/.bashrc
 echo "alias gg='ripper-tags -R --exclude=.git --exclude=vendor .'" >> ~/.bashrc
 echo "alias ivm='vim'" >> ~/.bashrc
 echo "alias rc='vim ~/.bashrc && source ~/.bashrc'" >> ~/.bashrc
+echo "alias tf='tail -f log/*'" >> ~/.bashrc
 echo "export RAILS_ENV=" >> ~/.bashrc
 vim ~/.bashrc && source ~/.bashrc
